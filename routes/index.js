d99d67fdda14236b4cf3a0df5eb99949d109bf7d
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-05-08 15:35:41 -0700

    good soup

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,42 @@ var express = require('express');
 const Logger = require('nodemon/lib/utils/log');
 var router = express.Router();
 const axios = require('axios');
-const URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
+var url = require('url');
+let liquor;
+const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=`
 
+
+function getPage(liquorChoice, res) {liquor = liquorChoice;
+  console.log(liquor)
+axios.get(`${URL}${liquor}`)
+.then(response => {
+  const drinkData = response.data;
+  res.send(drinkData)
+})
+.catch(error => {
+  console.log(error);
+});
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/drinks', (req, res, next) => {
-  axios.get(URL)
-  .then(response => {
-    res.send(response.data);
-    console.log(response.data);
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+router.get('/api/drinks/vodka', (req, res, next) => {
+  const address = url.parse(req.url, true).path.split('/')[3]
+  getPage(address, res)
+})
+router.get('/api/drinks/tequila', (req, res, next) => {
+  const address = url.parse(req.url, true).path.split('/')[3]
+  getPage(address, res)
+})
+router.get('/api/drinks/rum', (req, res, next) => {
+  const address = url.parse(req.url, true).path.split('/')[3]
+  getPage(address, res)
+})
+router.get('/api/drinks/gin', (req, res, next) => {
+  const address = url.parse(req.url, true).path.split('/')[3]
+  getPage(address, res)
 })
 
 module.exports = router;
