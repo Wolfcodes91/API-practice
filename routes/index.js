82c52e6b320b9118e7003cacd75aa65399d58da7
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-05-17 18:37:58 -0700

    works better but glitchy

--- routes/index.js
+++ routes/index.js
@@ -1,113 +1,67 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-var url = require('url');
+// var url = require('url');
 const _ = require('lodash')
-let liquor;
-let drinkData = [];
 const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=`;
+let drinkData = []
 
-function axeHit(liquor) {
-  axios.get(`${URL}${liquor}`)
-.then(response => {
-  drinkData.push(response.data.drinks)
-})
-.catch(error => {
-  console.log(error);
-});
+function axeHit(liquorChoice) {
+  console.log(liquorChoice)
+  liquorChoice.forEach(async (arg) => {
+    axios.get(`${URL}${arg}`)
+    .then(response => {
+      drinkData.push(response.data.drinks)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  })
+return drinkData
 }
 
-function getPage(liquorChoice, res) {liquor = liquorChoice;
-  axios.get(`${URL}${liquor}`)
-.then(response => {
-  const drinkData = response.data;
-  res.send(drinkData)
-})
-.catch(error => {
-  console.log(error);
-});
-}
-
-function loop(liquorChoice, req) {
-  for (let i = 0; i < Object.keys(req.params).length; i++) {
-    liquorChoice.push(req.params[`drinkId${i}`])
+function getPage(res, liquorChoice) {
+  axeHit(liquorChoice)
+  let semifinalDrinkData = drinkData;
+  if (drinkData.length === 1) {
+    res.send(drinkData)
+    return
   }
-}
-
-function getPageAdvanced(liquor1, liquor2, res) {
-axios.get(`${URL}${liquor1}`)
-axeHit(liquor1)
-axios.get(`${URL}${liquor2}`)
-.then(response => {
-  drinkData.push(response.data.drinks)
-  let semifinalDrinkData = drinkData[0].concat(drinkData[1])
+  else if (drinkData.length === 2) {
+    semifinalDrinkData = drinkData[0].concat(drinkData[1])
+  }
+  else if (drinkData.length === 3) {
+    semifinalDrinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2])
+  }
+  else if (drinkData.length === 4) {
+    semifinalDrinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2]).concat(drinkData[3])
+  }
   let sortedDrinks = _.sortBy(semifinalDrinkData, 'strDrink')
-  let finalDrinkData = {drinks: sortedDrinks}
+  const uniqueDrinkData = sortedDrinks.reduce((acc, current) => {
+    const x = acc.find(item => item['strDrink'] === current['strDrink']);
+    if (!x) {
+      return acc.concat([current]);
+    } else {
+      return acc;
+    }
+  }, []);
+  let finalDrinkData = {drinks: uniqueDrinkData}
   res.send(finalDrinkData)
-})
-.catch(error => {
-  console.log(error);
-});
-}
-
-function getPageSuperAdvanced(liquor1, liquor2, liquor3, res) {
-axeHit(liquor1)
-axeHit(liquor2)
-axios.get(`${URL}${liquor3}`)
-.then(response => {
-  drinkData.push(response.data.drinks)
-  let semifinalDrinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2])
-  let sortedDrinks = _.sortBy(semifinalDrinkData, 'strDrink')
-  let finalDrinkData = {drinks: sortedDrinks}
-  res.send(finalDrinkData)
-})
-.catch(error => {
-  console.log(error);
-});
-}
-
-function getPageSuperDuperAdvanced(liquor1, liquor2, liquor3, liquor4, res) {
-axeHit(liquor1)
-axeHit(liquor2)
-axeHit(liquor3)
-axios.get(`${URL}${liquor4}`)
-.then(response => {
-  drinkData.push(response.data.drinks)
-  let semifinalDrinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2]).concat(drinkData[3])
-  let sortedDrinks = _.sortBy(semifinalDrinkData, 'strDrink')
-  let finalDrinkData = {drinks: sortedDrinks}
-  res.send(finalDrinkData)
-})
-.catch(error => {
-  console.log(error);
-});
 }
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/drinks/:drinkId/', (req, res) => {
-  let liquorChoice = req.params.drinkId
-  getPage(liquorChoice, res)
-})
-
-router.get('/api/drinks/:drinkId0/:drinkId1', (req, res) => {
-  let liquorChoice = [];
-  loop(liquorChoice, req)
-  getPageAdvanced(liquorChoice[0], liquorChoice[1], res)
-})
-
-router.get('/api/drinks/:drinkId0/:drinkId1/:drinkId2', (req, res) => {
-  let liquorChoice = [];
-  loop(liquorChoice, req)
-  getPageSuperAdvanced(liquorChoice[0], liquorChoice[1], liquorChoice[2], res)
-})
-
-router.get('/api/drinks/:drinkId0/:drinkId1/:drinkId2/:drinkId3', (req, res) => {
-  let liquorChoice = [];
-  loop(liquorChoice, req)
-  getPageSuperDuperAdvanced(liquorChoice[0], liquorChoice[1], liquorChoice[2], liquorChoice[3], res)
+router.get('/api/drinks', async (req, res) => {
+  const {tags} = req.query
+  if (tags) { 
+    let liquorChoice = tags.split(',')
+    await getPage(res, liquorChoice)
+  }
+  else {
+    console.error('no tags exist')
+  }
 })
 
 module.exports = router;
