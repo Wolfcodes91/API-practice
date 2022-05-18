fd31506686497248dd9e05fb9b8850f75735a39d
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-05-17 22:36:32 -0700

    cleaner but still not working perfectly

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,37 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-// var url = require('url');
-const _ = require('lodash')
+const _ = require('lodash');
 const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=`;
-let drinkData = []
+let drinkData = [];
 
-function axeHit(liquorChoice) {
-  console.log(liquorChoice)
-  liquorChoice.forEach(async (arg) => {
-    axios.get(`${URL}${arg}`)
-    .then(response => {
-      drinkData.push(response.data.drinks)
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  })
-return drinkData
+router.get('/api/drinks', (req, res) => {
+  const {tags} = req.query
+  if (tags) { 
+    const liquorChoice = tags.split(',')
+    getPage(liquorChoice, res)
+  }
+  else {
+    errorMessage(res)
+  }
+})
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+function errorMessage(res) {
+  res.send(
+    {
+    "message": "no tags exist",
+    }
+    )
+    console.error('no tags exist')
 }
 
-function getPage(res, liquorChoice) {
+function getPage(liquorChoice, res) {
   axeHit(liquorChoice)
-  let semifinalDrinkData = drinkData;
-  if (drinkData.length === 1) {
-    res.send(drinkData)
-    return
-  }
-  else if (drinkData.length === 2) {
-    semifinalDrinkData = drinkData[0].concat(drinkData[1])
-  }
-  else if (drinkData.length === 3) {
-    semifinalDrinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2])
-  }
-  else if (drinkData.length === 4) {
-    semifinalDrinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2]).concat(drinkData[3])
-  }
-  let sortedDrinks = _.sortBy(semifinalDrinkData, 'strDrink')
+  const sortedDrinks = _.sortBy(concatData(drinkData), 'strDrink')
   const uniqueDrinkData = sortedDrinks.reduce((acc, current) => {
     const x = acc.find(item => item['strDrink'] === current['strDrink']);
     if (!x) {
@@ -49,19 +44,29 @@ function getPage(res, liquorChoice) {
   res.send(finalDrinkData)
 }
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+function axeHit(liquorChoice) {
+  liquorChoice.forEach((arg) => {
+    axios.get(`${URL}${arg}`)
+    .then(response => {
+      drinkData.push(response.data.drinks)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  })
+}
 
-router.get('/api/drinks', async (req, res) => {
-  const {tags} = req.query
-  if (tags) { 
-    let liquorChoice = tags.split(',')
-    await getPage(res, liquorChoice)
+function concatData(drinkData) {
+  if (drinkData.length === 2) {
+    drinkData = drinkData[0].concat(drinkData[1])
   }
-  else {
-    console.error('no tags exist')
+  else if (drinkData.length === 3) {
+    drinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2])
   }
-})
+  else if (drinkData.length === 4) {
+    drinkData = drinkData[0].concat(drinkData[1]).concat(drinkData[2]).concat(drinkData[3])
+  }
+  return drinkData
+}
 
 module.exports = router;
